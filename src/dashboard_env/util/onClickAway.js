1222954ab2e5e73a0ff0bd418eb932680eda19df
Obbik <norbertdering@gmail.com>
2021-02-16 13:32:22 +0100

    escape key function added

--- src/dashboard_env/util/onClickAway.js
+++ src/dashboard_env/util/onClickAway.js
@@ -1,4 +1,16 @@
 export default (ref, handleClickAway) => {
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    let isEscape = false;
+    if ("key" in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+      isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+      handleClickAway()
+    }
+  };
   const eventWrapper = evt => {
     if (ref.current && !ref.current.contains(evt.target)) {
       handleClickAway()
