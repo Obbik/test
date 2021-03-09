ee204a2301c6c68060ec8f21bf7d4b53ae107cc6
Obbik <norbertdering@gmail.com>
2021-03-09 14:49:09 +0100

    translation

--- src/dashboard_env/components/Modals/AcceptForm.js
+++ src/dashboard_env/components/Modals/AcceptForm.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react'
-
+import { LangContext } from '../../context/lang-context'
 import FormSkel from './FormSkel'
 
 export default ({ deleteProduct, handleClose, ean, IsSubscribed, unsubscribeProduct, categoryId, deleteCategory, deleteLabel }) => {
+    const { TRL_Pack } = useContext(LangContext)
     const HandleDelete = evt => {
         evt.preventDefault()
         if (!deleteLabel)
             !categoryId ? (!IsSubscribed ? deleteProduct(ean) : unsubscribeProduct(ean)) : (deleteCategory(categoryId))
         else deleteLabel()
         handleClose()
+    }
+    const displayMessage = () => {
+        if (deleteLabel) {
+            return TRL_Pack.modalDisplays.deleteTag
+        }
+        else if (categoryId) {
+            return TRL_Pack.modalDisplays.deleteCategory
+        }
+        else if (IsSubscribed) {
+            return TRL_Pack.modalDisplays.unsubscribeProduct
+        }
+        else {
+            return `${TRL_Pack.modalDisplays.delEan}: ${ean}`
+        }
     }
     return (
         < FormSkel
@@ -20,10 +35,7 @@ export default ({ deleteProduct, handleClose, ean, IsSubscribed, unsubscribeProd
                 <form onSubmit={HandleDelete} id="modal-form">
                     <div >
                         <label >
-
-                            {!categoryId ? (IsSubscribed ? "Potwierdz odsubskrybowanie produktu" :
-                                `zaakceptowanie spowoduje usuniecie o Eanie : ${ean}`) : ("Potwierdź usunięcie kategori")
-                            }
+                            {displayMessage()}
                         </label>
                     </div>
                 </form>
